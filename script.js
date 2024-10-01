5cebb20c651ddca20f3807b3eb6147c571bcb392
Gabriel <josegabrielas0586@gmail.com>
2024-09-30 23:58:57 -0300

    Adições de responsividade

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ function corCirculo(cor){
     const circulo = document.querySelector('.banner-ball')
     circulo.style.background = cor
 }
+
+
+//mobile
+var barMobile = document.querySelector('#barMobile')
+barMobile.addEventListener('click', function(){
+    var listMobile = document.querySelector('#listMobile')
+    listMobile.classList.toggle('active')
+})
